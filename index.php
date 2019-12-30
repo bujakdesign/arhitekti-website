64a45642baa803b1793c360b3d9994b21a3fcb4d
BujakDesign <40665923+nbujak@users.noreply.github.com>
2019-12-30 13:22:28 +0100

    Update indexa, podsesen navbar.

--- index.php
+++ index.php
@@ -1,17 +1,78 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="ba" dir="ltr">
   <head>
+
     <meta charset="utf-8">
-    <title>4+ Aрхитекти</title>
+
+    <title>4+ Архитекти</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Kube CSS -->
+    <link rel="stylesheet" href="assets/css/kube.css">
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    
   </head>
   <body>
-
-    <div id="done 2019">Gotov projekat - 2019</div>
-    <div id="done 2018">Gotov projekat - 2018</div>
-    <div id="done 2017">Gotov projekat - 2017</div>
-    <div id="done 2016">Gotov projekat - 2016</div>
     
+    <div class="page">
+
+      <header class="header min-height">
+        <div class="container" style="padding-left: 0px ">
+
+          <div class="is-navbar-container">
+            <div class="is-brand">
+                <img src="assets/img/navbar-logo.png" class="is-logo" alt="Logo of the 4+Architects group.">              
+                <!-- nav toggle element -->
+                <a href="#"
+                        class="nav-toggle is-push-right-mobile is-shown-mobile icon-kube-menu"
+                        data-kube="toggle"
+                        data-target="#navbar"></a>
+            </div>
+            <!-- collapsable navigation -->
+            <div id="navbar" class="is-navbar is-hidden-mobile">
+                <nav class="is-push-right">
+                    <ul>
+                        <li>
+                          <a href="#" class="navbar-link">Пројекти</a>
+                        </li>
+                        <li>
+                          <a href="#" class="navbar-link">Студио 4+</a>
+                        </li>
+                        <li>
+                          <a href="#" class="navbar-link">Инфо</a>
+                        </li>
+                        <li>
+                          <a href="#" class="navbar-link">Контакт</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        </div>
+      </header>
+
+      <main class="main">
+
+      </main>
+
+      <footer class="footer">
+      
+      </footer>
+
+    </div>
+
+    <!-- Kube JS (optional if you are using Kube CSS only) -->
+    <script src="assets/js/kube.min.js"></script>
+    <script>
+      $K.init();
+    </script>
     <script src="assets/js/script.js"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
   </body>
 </html>
+
